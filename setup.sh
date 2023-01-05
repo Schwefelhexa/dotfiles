de466783f05877fe3dd8a2a41a70ac1e362ba6bb
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-01-05 11:14:14 +0100

    Add bspwm/sxhkd

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 mkdir $HOME/.config
 
 # Symlink to $HOME/.config
-configdirs=(alacritty btop nvim ranger)
+configdirs=(alacritty btop nvim ranger bspwm sxhkd)
 for d in "${configdirs[@]}"
 do
   target=$HOME/.config/$d 
