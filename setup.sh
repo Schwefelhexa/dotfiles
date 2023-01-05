f4be777ef635af2fa7a6a41179c301d98302a196
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-01-05 11:00:20 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+mkdir $HOME/.config
+
+# Symlink to $HOME/.config
+configdirs=(alacritty btop nvim ranger)
+for d in "${configdirs[@]}"
+do
+  target=$HOME/.config/$d 
+  [ -f $target ] && unlink $target || rm -rf $target
+  ln -s $HOME/.files/$d $target
+done
+
+# Symlink to $HOME
+homedirs=(ssh tmux.conf xinitrc zshrc)
+for d in "${homedirs[@]}"
+do
+  target=$HOME/.$d 
+  [ -f $target ] && unlink $target || rm -rf $target
+  ln -s $HOME/.files/$d $target
+done
+
