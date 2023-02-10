f3ebba3f9641e477ff5544f262ba6156b3ee8d07
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-02-10 09:14:36 +0100

    Add Copilot

--- nvim/lua/albarn/packer.lua
+++ nvim/lua/albarn/packer.lua
@@ -94,6 +94,8 @@ return require('packer').startup(function(use)
     }
   }
 
+  use "github/Copilot.vim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
