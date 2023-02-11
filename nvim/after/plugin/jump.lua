9b6bd91babb888d19943714e129cff9a512272e8
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-02-11 18:38:41 +0100

    Change harpoon quick jump keys

--- nvim/after/plugin/jump.lua
+++ nvim/after/plugin/jump.lua
@@ -4,10 +4,10 @@ local ui = require("harpoon.ui")
 vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = "[H]arpoon [A]dd" })
 vim.keymap.set('n', '<leader>hf', ui.toggle_quick_menu, { desc = "[H]arpoon [F]ind" })
 
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>hh", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>hj", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>hk", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>hl", function() ui.nav_file(4) end)
 
 require('leap').add_default_mappings()
 
