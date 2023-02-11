043e30676c818b3b56268ddb8a57973c2b2603d7
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-02-11 18:52:59 +0100

    nvim: Clean up packer config

--- nvim/lua/albarn/packer.lua
+++ nvim/lua/albarn/packer.lua
@@ -21,67 +21,16 @@ vim.cmd([[
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  -- Navigation
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.x',
     requires = { 'nvim-lua/plenary.nvim' }
   }
-
-  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-  use("theprimeagen/harpoon")
-  use("mbbill/undotree")
-  use("nvim-treesitter/nvim-treesitter-context")
-
-  use("lewis6991/gitsigns.nvim")
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
-
-  use 'ThePrimeagen/refactoring.nvim'
-
-  use {
-    "sudormrfbin/cheatsheet.nvim",
-    requires = {
-      { 'nvim-lua/popup.nvim' }
-    }
-  }
-
-  use "tversteeg/registers.nvim"
-  use "zbirenbaum/neodim"
-  use "rcarriga/nvim-notify"
-
-  use {
-    'glepnir/dashboard-nvim',
-    event = 'VimEnter',
-    config = function ()
-      require("albarn.dashboard")
-    end,
-    requires = { 'nvim-tree/nvim-web-devicons' }
-  }
-
-  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
   use 'ggandor/leap.nvim'
+  use("theprimeagen/harpoon")
 
-  use "folke/trouble.nvim"
-  use "folke/todo-comments.nvim"
-
-  use 'm4xshen/autoclose.nvim'
-  use 'voldikss/vim-floaterm'
-
-  use { 'catppuccin/nvim', as = 'catppuccin' }
-
-  use 'kyazdani42/nvim-web-devicons'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-
-  use {
-    'nvim-neo-tree/neo-tree.nvim',
-    requires = { { "MunifTanjim/nui.nvim", module = "nui" }, "nvim-tree/nvim-web-devicons" },
-  }
-
-  use "folke/which-key.nvim"
-
+  -- LSP
   use {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v1.x',
@@ -108,9 +57,59 @@ return require('packer').startup(function(use)
     }
   }
 
-  use "github/Copilot.vim"
+  -- Basic UI
+  use 'voldikss/vim-floaterm'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {
+    'nvim-neo-tree/neo-tree.nvim',
+    requires = { { "MunifTanjim/nui.nvim", module = "nui" }, "nvim-tree/nvim-web-devicons" },
+  }
 
+  -- Improved UI
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use("nvim-treesitter/nvim-treesitter-context")
+  use "folke/todo-comments.nvim"
+  use "folke/trouble.nvim"
+  use("lewis6991/gitsigns.nvim")
+  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
+  use {
+    "sudormrfbin/cheatsheet.nvim",
+    requires = {
+      { 'nvim-lua/popup.nvim' }
+    }
+  }
+  use {
+    'glepnir/dashboard-nvim',
+    event = 'VimEnter',
+    config = function ()
+      require("albarn.dashboard")
+    end,
+    requires = { 'nvim-tree/nvim-web-devicons' }
+  }
+  use("mbbill/undotree")
+
+  -- Utilities
+  use "tversteeg/registers.nvim"
+  use 'm4xshen/autoclose.nvim'
+  use "folke/which-key.nvim"
+  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
+
+  -- Colorschemes
+  use "zbirenbaum/neodim"
+  use { 'catppuccin/nvim', as = 'catppuccin' }
+
+  -- Silly stuff
   use "seandewar/killersheep.nvim"
+
+  -- Dependencies
+  use "rcarriga/nvim-notify"
+  use 'kyazdani42/nvim-web-devicons'
+
+  -- Copilot
+  use "github/Copilot.vim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
