fa707bb085387957d253dc58f1a5755cb38bf6d2
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-02-28 23:21:56 +0100

    nvim: Dad-joke greeting

--- nvim/after/plugin/greeter.lua
+++ nvim/after/plugin/greeter.lua
@@ -1,10 +1,23 @@
+local function eval(command, fallback)
+  local handle = io.popen(command)
+  if not handle then
+    return fallback
+  end
+
+  local result = handle:read("*a")
+  handle:close()
+  return result
+end
+
+local header = eval("botsay -c $(curl --no-progress-meter https://icanhazdadjoke.com)", "No botsay, no fun :(")
+local stats = require("lazy").stats()
+
 require("mini.starter").setup({
-  header = "This is a header",
+  header = header,
   items = {
     { name = "Item 1", section = "Section A", action = "" },
     { name = "Item 2", section = "Section A", action = "" },
     { name = "Item 3", section = "Section A", action = "" },
   },
-  footer = "This is a footer"
+  footer = "🚀 Loaded " .. stats.loaded .. " plugins. Blazingly fast! 🚀"
 })
-
