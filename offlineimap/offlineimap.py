9107d46456c7e17a39c1c325083583785b3f1651
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-03-12 13:06:02 +0100

    Offlineimap: fix 1Password path bin (again)

--- offlineimap/offlineimap.py
+++ offlineimap/offlineimap.py
@@ -5,20 +5,20 @@ import os
 
 def get_password(name):
     val = os.popen(
-        "/usr/bin/op item get \"{}\" --fields label=password".format(name)).read()
+        "/usr/local/bin/op item get \"{}\" --fields label=password".format(name)).read()
     val = val[:-1]  # remove trailing newline
     return val
 
 
 def get_username(name):
     val = os.popen(
-        "/usr/bin/op item get \"{}\" --fields label=username".format(name)).read()
+        "/usr/local/bin/op item get \"{}\" --fields label=username".format(name)).read()
     val = val[:-1]  # remove trailing newline
     return val
 
 
 def get_hostname(name):
     val = os.popen(
-        "/usr/bin/op item get \"{}\" --fields label=host".format(name)).read()
+        "/usr/local/bin/op item get \"{}\" --fields label=host".format(name)).read()
     val = val[:-1]  # remove trailing newline
     return val
