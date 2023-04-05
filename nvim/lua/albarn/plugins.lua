b126ba118208235b01a6fa8572d81250725e8184
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-04-05 15:26:55 +0200

    nvim: neoconf

--- nvim/lua/albarn/plugins.lua
+++ nvim/lua/albarn/plugins.lua
@@ -30,22 +30,27 @@ local plugins = {
     branch = "v1.x",
     dependencies = {
       -- LSP Support
-      "neovim/nvim-lspconfig",
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
+      { 'neovim/nvim-lspconfig' },         -- Required
+      { 'williamboman/mason.nvim' },       -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
       -- Autocompletion
-      "hrsh7th/nvim-cmp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lua",
+      { 'hrsh7th/nvim-cmp' },     -- Required
+      { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+      { 'hrsh7th/cmp-buffer' },   -- Optional
+      { 'hrsh7th/cmp-path' },     -- Optional
+      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },         -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
 
       -- Status info
       "j-hui/fidget.nvim",
     },
   },
+  "folke/neoconf.nvim",  -- Per-project configuration
 
   -- Basic UI
   "voldikss/vim-floaterm",
