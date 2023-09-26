31c53b0980ccf8d889d8df3da3bb657ccfcf5749
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-09-26 13:49:05 +0200

    setup: include karabiner

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 mkdir $HOME/.config
 
 # Symlink to $HOME/.config
-configdirs=(alacritty btop nvim ranger bspwm sxhkd polybar offlineimap notmuch alot msmtp spicetify starship.toml i3 picom.conf lazygit rofi bat)
+configdirs=(alacritty btop nvim ranger bspwm sxhkd polybar offlineimap notmuch alot msmtp spicetify starship.toml i3 picom.conf lazygit rofi bat karabiner)
 for d in "${configdirs[@]}"
 do
   target=$HOME/.config/$d
