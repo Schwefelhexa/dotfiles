312ee08116b6fdc70994b72da86760facdadabb3
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-10-12 15:52:26 +0200

    nvim: more cellular automaton shortcuts

--- nvim/after/plugin/cellular_automaton.lua
+++ nvim/after/plugin/cellular_automaton.lua
@@ -1,1 +1,3 @@
 vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
+vim.keymap.set("n", "<leader>ffs", "<cmd>CellularAutomaton scramble<CR>")
+vim.keymap.set("n", "<leader>jfc", "<cmd>CellularAutomaton game_of_life<CR>")
