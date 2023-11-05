293927f49617a553fce08a9dde0d7dbeee433802
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-11-05 15:44:53 +0100

    nvim: explicit highlight callback for treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,5 +1,17 @@
 require("nvim-treesitter.configs").setup({
 	auto_install = true,
+	highlight = {
+		enable = true,
+		disable = function(_, buf)
+			-- disable treesitter for large files
+			local max_filesize = 100 * 1024   -- 100 KB
+			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+			if ok and stats and stats.size > max_filesize then
+				print("File too large, disabling TreeSitter hightlights")
+				return true
+			end
+		end,
+	},
 	autotag = {
 		enable = true,
 	},
