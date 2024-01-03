baa5e6d0a1996db7715f5249afea6840f9dc3819
Alexander Baron <alexander.baron1@rwth-aachen.de>
2024-01-03 13:01:09 +0100

    nvim: fix transparent background for current buffer fuzzy find

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -16,7 +16,6 @@ local telescope = require("telescope.builtin")
 vim.keymap.set("n", "<leader>/", function()
 	-- You can pass additional configuration to telescope to change theme, layout, etc.
 	telescope.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown {
-		winblend = 10,
 		previewer = false,
 	})
 end, { desc = "[/] Fuzzily search in current buffer" })
