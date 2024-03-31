1d8a796e8c37c87c1a2962f2b21a48f1b63ad7fa
Alexander Baron <alexander.baron1@rwth-aachen.de>
2024-03-31 10:20:25 +0200

    nvim: show copilot status

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,6 +1,28 @@
+local function copilot_status()
+	local client = require("copilot.client")
+
+	local status = "Copilot "
+
+	if client.is_disabled() then
+		status = status .. " "
+	else
+		status = status .. " "
+	end
+
+	if not client.buf_is_attached(0) then
+		status = status .. " "
+	end
+
+	return status
+end
+
 require("lualine").setup({
 	options = {
 		component_separators = { left = "|", right = "|" },
 		section_separators = { left = "", right = "" },
+	},
+	sections = {
+		lualine_c = { 'filename', copilot_status },
+		lualine_x = { 'encoding', 'filesize', 'fileformat', 'filetype' }
 	}
 })
